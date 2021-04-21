b3bb8dfc458fd7a1b144451d55d00192dcf828ba
Rasmus Nilsson <info@rasmusnilsson.se>
2021-04-21 14:27:22 +0200

    added more assertions to test

--- src/services/__test__/compile-mood.test.js
+++ src/services/__test__/compile-mood.test.js
@@ -2,9 +2,10 @@ const {config} = require("../../domain/mood")
 const compileMood = require("../compile-mood")
 
 it("fetches data with appropriate conditions", async () => {
+    let findWasCalled = false
     config.model = {
         find: params => {
-            expect(params.user).toEqual(1234)
+            findWasCalled = true
             return [
                 {
                     user: 1234,
@@ -25,4 +26,7 @@ it("fetches data with appropriate conditions", async () => {
     const result = await compileMood(1234, "2016-01-02", "2016-02-03")
     expect(result.averageMood).toEqual(2)
     expect(result.mostCommonTags[0]).toEqual("happy")
+    expect(result.reportAmount).toEqual(2)
+    expect(result.happiness).toEqual(0)
+    expect(findWasCalled).toEqual(true)
 })
